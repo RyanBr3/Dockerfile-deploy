ec0893a16e90a29be0876a86bcb3f42d96a3eb4f
“ryandeoliveira” <ryandomingosoliveira@gmail.com>
2023-10-14 20:11:20 -0300

    Dockerfile deploy

---
+++ src/test/java/br/com/ryandeoliveira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ryandeoliveira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
